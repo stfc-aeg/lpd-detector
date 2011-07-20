d1ad82363cedd32e44ac14c084923dcf64894c41
tcn45 <tcn45>
2011-07-20 12:28:03 +0000

    Added looping over transaction length and table output of results

--- python/src/femTests/FemTransactionTest.py
+++ python/src/femTests/FemTransactionTest.py
@@ -1,7 +1,28 @@
+import sys
+import os
 import time
 
 from femClient.FemClient import  FemClient
 from femApi.femTransaction import FemTransaction
+
+# Make stdout unbuffered
+sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
+
+# Transaction parameters
+#addr = 0x81440000  # LEDs
+addr = 0x8b900000  # SRAM
+lengthArr = [1, 4, 8, 16, 32, 64, 128, 256]
+numLoops = 100
+bus = FemTransaction.BUS_RAW_REG
+width = FemTransaction.WIDTH_LONG
+
+
+# Lists for results
+writeDeltaT = []
+writeBytes  = []
+readDeltaT = []
+readBytes  = []
+
 
 # Open FEM connection
 
@@ -12,41 +33,57 @@ theFem = FemClient((host, port))
 
 if theFem != None:
     print "Connected OK..."
+else:
+    print "ERROR: connection failed"    
+    sys.exit(1)
     
-    
-# Setup parameters for write transaction test
-bus = FemTransaction.BUS_RAW_REG
-width = FemTransaction.WIDTH_LONG
-state = FemTransaction.STATE_WRITE
-addr = 0x81440000
-numLoops = 1000
+# Write transaction test
+print "Running write transaction tests",
+for length in lengthArr:
 
-print "Starting write transaction test, num loops =", numLoops
-start = time.time()
-for i in range(0, numLoops):
-    values = i%256,
-    ack = theFem.write(bus, width, addr, values)
-end = time.time()
-deltaT = end - start
-print "Done: %d writes took %.3f secs, rate = %.1f per sec " % (numLoops, deltaT, numLoops / deltaT)
-    
-# Setup parameters for read transaction test
-bus = FemTransaction.BUS_RAW_REG
-width = FemTransaction.WIDTH_LONG
-state = FemTransaction.STATE_WRITE
-addr = 0x81440000
-length = 1
-numLoops = 1000
+    (payloadByteLen,payloadFormat) = FemTransaction.widthEncoding[width]
+    totalBytes = payloadByteLen * length * numLoops
+    start = time.time()
+    for i in range(0, numLoops):
+        values = tuple(range(0, length))
+        ack = theFem.write(bus, width, addr, values)
+    end = time.time()
+    deltaT = end - start
+    writeDeltaT.append(deltaT)
+    writeBytes.append(totalBytes)
+    print ".",
 
-print "Starting read transaction test, num loops =", numLoops
-start = time.time()
-for i in range(0, numLoops):
-    values = theFem.read(bus, width, addr, length)
-end = time.time()
-deltaT = end - start
-print "Done: %d read took %.3f secs, rate = %.1f per sec " % (numLoops, deltaT, numLoops / deltaT)
+print " done."    
     
+# Read transaction test
+print "Running read transaction tests ",
+for length in lengthArr:
+
+    (payloadByteLen,payloadFormat) = FemTransaction.widthEncoding[width]
+    totalBytes = payloadByteLen * length * numLoops
+    start = time.time()
+    for i in range(0, numLoops):
+        values = theFem.read(bus, width, addr, length)
+    end = time.time()
+    deltaT = end - start
+    readDeltaT.append(deltaT)
+    readBytes.append(totalBytes)
+    print ".",
+    
+print " done."
+
+# Print results
+print ""
+print "                    Write                          Read"
+print "Length    Time      Rate        Thru     Time      Rate        Thru"
+print "--------------------------------------------------------------------"
+
+for i in range(0, len(lengthArr)): 
+    print "%5d     %5.3f     %5.1f     %7.1f    %5.3f     %5.1f     %7.1f" % \
+        (lengthArr[i], writeDeltaT[i], numLoops / writeDeltaT[i], writeBytes[i] / writeDeltaT[i],
+         readDeltaT[i], numLoops / readDeltaT[i], readBytes[i] / readDeltaT[i]) 
+
 # Close the connection
-
 theFem.close()
+
 
