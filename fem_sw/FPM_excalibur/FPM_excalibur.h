7dc8029b69f11e195dad4cb0c3cd4517b5685249
mt47 <mt47>
2012-01-11 09:34:08 +0000

    Added FEM Personality Module structure (FPM)

---
+++ fem_sw/FPM_excalibur/FPM_excalibur.h
@@ -0,0 +1,19 @@
+/*
+ * FPM_excalibur.h
+ *
+ * Application specific functions for Excalibur FEM
+ *
+ */
+
+#ifndef FPM_EXCALIBUR_H_
+#define FPM_EXCALIBUR_H_
+
+#include "personality.h"
+
+// TODO: Move these to personality.h!
+#include "xmk.h"
+#include "stdio.h"
+#include "xstatus.h"
+#include <fem.h>
+
+#endif /* FPM_EXCALIBUR_H_ */
---
+++ fem_sw/FPM_lpd/FPM_lpd.h
@@ -0,0 +1,19 @@
+/*
+ * FPM_lpd.h
+ *
+ * Application specific functions for XFEL LPD
+ *
+ */
+
+#ifndef FPM_LPD_H_
+#define FPM_LPD_H_
+
+#include "personality.h"
+
+// TODO: Move these to personality.h!
+#include "xmk.h"
+#include "stdio.h"
+#include "xstatus.h"
+#include <fem.h>
+
+#endif /* FPM_LPD_H_ */
