99c86cb44f564c0ab9d281c6f148ccd2e05b8fb2
ckd27546 <ckd27546>
2012-09-11 12:53:41 +0000

    Updated file to correct dictionary values and included unit testing.

--- python/src/LpdCommandSequence/SlowCtrlParams.py
+++ python/src/LpdCommandSequence/SlowCtrlParams.py
@@ -1,6 +1,15 @@
 # Check in setParaValue() for list/integer type 
 import types
 
+# Define Exception Class
+class SlowCtrlParamsInvalidRangeError(Exception):
+    
+    def __init__(self, msg):
+        self.msg = msg
+        
+    def __str__(self):
+        return repr(self.msg)
+
 class SlowCtrlParams(object):
  
     numPixels = 3
@@ -14,16 +23,15 @@ class SlowCtrlParams(object):
                   'self_test_enable'            : [1, 3584, -1],
                   'daq_bias_default'            : [5, -1, -1],
                   'daq_bias'                    : [5, 3585, [-1] * 47],
-                  'spare_bits'                  : [5, 3820, [-1] * 4],  # Special Case: 5 bits cover everything
+                  'spare_bits'                  : [5, 3820, -1],        # Special Case: 5 bits cover everything
                   '100x_filter_control'         : [20, 3825, -1],       # Special Case: 20 bits cover everything
                   'adc_clock_delay'             : [20, 3845, -1],       # Special Case: 20 bits cover everything
-                  'digital_control'             : [20, 3865, [-1] * 6], # Special Case: 40 bits cover everything
+                  'digital_control'             : [40, 3865, -1],       # Special Case: 40 bits cover everything
                   }
      
     def __init__(self):
         
-        self.pixelGain = range(SlowCtrlParams.numPixels)
-        self.pixelGain2 = range(SlowCtrlParams.numPixels)
+        pass
 
     def getParamValue(self, dictKey):
         """ Obtains the three variables associated with the dictionary key 'dictKey' 
@@ -43,7 +51,7 @@ class SlowCtrlParams(object):
         else:
             print "\"" + dictKey + "\" doesn't exist."
         
-        return width, posn, val
+        return [width, posn, val]
  
     def setParamValue(self, dictKey, index, value):
         """ updates the dictionary key 'dictKey' with the new value in 'value', 
@@ -51,59 +59,176 @@ class SlowCtrlParams(object):
             
         # Does dictKey exist in the dictionary?
         if dictKey in self.paramsDict:
-            # Save the old values that we want to retain
-            result = self.paramsDict[dictKey]
-            # Is the third variable a list or an integer?
-            if isinstance(result[2], types.ListType):
-                # it's a list; Remove the old value
-                result[2].pop(index)
-                # Insert the new value
-                result[2].insert(index, value)
-                
-            elif isinstance(result[2], types.IntType):
-                result[2] = value
+                        
+            # Save the current key's values that we (may) want to retain
+            currentValue = self.paramsDict[dictKey]
+            
+#            print "\trange check: ", 0, " value: ", value, " max: ", (2**currentValue[0])
+            # Check that the new value is within valid range
+            if not (0 <= value <= ( (2**currentValue[0]) -1) ):
+                raise SlowCtrlParamsInvalidRangeError("Key %s's new value outside valid range" % dictKey)
+#                print "\tnew value is outside valid range!"
+            
             else:
-                print "SetParamValue() Error: result[2] is neither a list nor an integer!"
-                                
-            # Overwrite the old value
-            self.paramsDict[dictKey] = result
+                # Is the third variable a list or an integer?
+                if isinstance(currentValue[2], types.ListType):
+                    # it's a list; Remove the old value
+                    currentValue[2].pop(index)
+                    # Insert the new value
+                    currentValue[2].insert(index, value)
+                    
+                elif isinstance(currentValue[2], types.IntType):
+                    currentValue[2] = value
+                else:
+                    print "SetParamValue() Error: currentValue[2] is neither a list nor an integer!"
+                                    
+                # Overwrite the old value
+                self.paramsDict[dictKey] = currentValue
 
         else:
             print "\"" + dictKey + "\" doesn't exist."
 
+import unittest
+    
+class SlowCtrlParamsTest(unittest.TestCase):
+    '''
+    Unit test class for SlowCtrlParams.
+    '''
+    
+    def testStringParse(self):
+        '''
+        Tests that updating a parameter works as expected
+        '''
+        
+        # setParamValue(dictKey, index, value)
+        dictKey = "self_test_decoder_default"
+        index = 0
+        value = 5
 
+
+        # Expected encoded values
+        expectedVals = [3, -1, 5]
+    
+        # Parse XML and encode
+        paramsObj = SlowCtrlParams()
+        paramsObj.setParamValue(dictKey, index, value)
+        
+        newVals = paramsObj.getParamValue(dictKey)
+
+        # Test that intended value matches expected
+        self.assertEqual(newVals, expectedVals, 'testStringParse() failed to update key as expected')
+
+        # 'daq_bias'                    : [5, 3585, [-1] * 47],
+        dictKey = 'daq_bias'
+        index = 5
+        value = 22
+        
+        expectedVals = paramsObj.getParamValue(dictKey)
+        expectedVals[2][index] = value
+        
+        paramsObj.setParamValue(dictKey, index, value)
+        newVals = paramsObj.getParamValue(dictKey)
+
+        self.assertEqual(newVals, expectedVals, 'testStringParse() failed to update key as expected')
+
+    def testOutOfRangeKeyValueFails(self):
+        '''
+        Tests that updating a parameter will fail if value exceeds valid range
+        '''
+        
+        # setParamValue(dictKey, index, value)
+        dictKey = "self_test_decoder_default"
+        index = 0
+        value = 8
+    
+        # Parse XML and encode
+        paramsObj = SlowCtrlParams()
+        with self.assertRaises(SlowCtrlParamsInvalidRangeError):
+            paramsObj.setParamValue(dictKey, index, value)
+
+        dictKey = "self_test_decoder"
+        with self.assertRaises(SlowCtrlParamsInvalidRangeError):
+            paramsObj.setParamValue(dictKey, index, value)
+
+        dictKey = "mux_decoder_default"
+        with self.assertRaises(SlowCtrlParamsInvalidRangeError):
+            paramsObj.setParamValue(dictKey, index, value)
+
+        dictKey = "mux_decoder"
+        with self.assertRaises(SlowCtrlParamsInvalidRangeError):
+            paramsObj.setParamValue(dictKey, index, value)
+        
+        # Test 1 bit widths
+        dictKey = "feedback_select_default"
+        value = 2
+        with self.assertRaises(SlowCtrlParamsInvalidRangeError):
+            paramsObj.setParamValue(dictKey, index, value)
+
+        dictKey = "feedback_select"
+        with self.assertRaises(SlowCtrlParamsInvalidRangeError):
+            paramsObj.setParamValue(dictKey, index, value)
+
+        dictKey = "self_test_enable"
+        with self.assertRaises(SlowCtrlParamsInvalidRangeError):
+            paramsObj.setParamValue(dictKey, index, value)
+
+        # Test 5 bit widths
+        dictKey = "daq_bias_default"
+        value = 32
+        with self.assertRaises(SlowCtrlParamsInvalidRangeError):
+            paramsObj.setParamValue(dictKey, index, value)
+
+        dictKey = "daq_bias"
+        with self.assertRaises(SlowCtrlParamsInvalidRangeError):
+            paramsObj.setParamValue(dictKey, index, value)
+
+        dictKey = "spare_bits"
+        with self.assertRaises(SlowCtrlParamsInvalidRangeError):
+            paramsObj.setParamValue(dictKey, index, value)
+
+        # Test 20 bit width
+        dictKey = "100x_filter_control"
+        value = 1048575+1
+        with self.assertRaises(SlowCtrlParamsInvalidRangeError):
+            paramsObj.setParamValue(dictKey, index, value)
+        
+        dictKey = "adc_clock_delay"
+        with self.assertRaises(SlowCtrlParamsInvalidRangeError):
+            paramsObj.setParamValue(dictKey, index, value)
+
+        # Test 40 bit width
+        dictKey = "digital_control"
+        value = 1099511627776
+        with self.assertRaises(SlowCtrlParamsInvalidRangeError):
+            paramsObj.setParamValue(dictKey, index, value)
+        
 if __name__ == '__main__':
     
-    theParams = SlowCtrlParams()
     
-#    theParams.getParamValue("mux_decoder_default")
-    width, posn, val = theParams.getParamValue("self_test_decoder_default")
-    print width, posn, val
+    # Execute unit testing
+    unittest.main()
     
-    print "Changing self_test_decoder_default.."
-    
-    theParams.setParamValue("self_test_decoder_default", 1, 56)
-    
-    print "the new value:"
-    width, posn, val = theParams.getParamValue("self_test_decoder_default")
-    print width, posn, val
+    ''' Manual testing '''
+#    theParams = SlowCtrlParams()
+#    
+##    theParams.getParamValue("mux_decoder_default")
+#    width, posn, val = theParams.getParamValue("self_test_decoder_default")
+#    print "Before: ", width, posn, val, "...", 
+#    
+#    print "Changing self_test_decoder_default.."
+#    theParams.setParamValue("self_test_decoder_default", 1, 56)
+#     
+#    width, posn, val = theParams.getParamValue("self_test_decoder_default")
+#    print "After: ", width, posn, val
+#
+#    print "-=-=-=-=-=-"
+#    
+#    width, posn, val = theParams.getParamValue("daq_bias")
+#    print "Before: ", width, posn, val, "...", 
+#    
+#    print "Changing daq_bias.."
+#    theParams.setParamValue("daq_bias", 11, 28)
+#    
+#    width, posn, val = theParams.getParamValue("daq_bias")
+#    print "After:  ", width, posn, val
 
-
-#    paramName = "pixelGain2"
-#    paramIdx  = 2
-#    
-#         
-#    print getattr(theParams, 'pixelGain')
-#        
-#    
-#    getattr(theParams, paramName)[paramIdx] = 321
-#        
-#    
-#    print getattr(theParams, paramName)
-#    print getattr(theParams, paramName)[2]
-#        
-#    
-#    print theParams.__dict__
-#        
-#    setattr(theParams, 'new', [3,2,1])
-#    print theParams.__dict__
