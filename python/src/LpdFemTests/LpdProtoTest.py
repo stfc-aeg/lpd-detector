dd6c364eb1d3f5ed4aad9e005583491968980a03
ckd27546 <ckd27546>
2012-10-09 13:55:55 +0000

    Initial template.

--- python/src/LpdFemTests/LpdProtoTest.py
+++ python/src/LpdFemTests/LpdProtoTest.py
@@ -1,6 +1,45 @@
 '''
+LpdProtoTest - XFEL LPD class sitting between the API and the FemC FemClient
+
+             - Will be renamed LpdFemClient at some point in the future
+            
 Created 09 October 2012
 
 @Author: ckd
 '''
 
+from FemClient.FemClient import *
+from FemApi.FemTransaction import FemTransaction
+
+class LpdProtoTest(FemClient):
+    '''
+    
+    '''
+    
+    
+    def __init__(self):
+        '''
+        Constructor for LpdProtoTest
+        '''
+            
+        pass
+        
+    def SensorATempGet(self, device, channel):
+        '''
+        Get temperature value (in ADC counts) of channel in device
+        '''
+        
+        pass
+    
+    def SensorATempSet(self, device, channel, value):
+        '''
+        Set temperature value of channel in device
+        '''
+        
+        pass
+    
+
+
+if __name__ == "__main__":
+    
+    print "hello"
