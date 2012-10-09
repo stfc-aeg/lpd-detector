8d1c02bac0d83944f19218af5f123240c15f8dee
ckd27546 <ckd27546>
2012-10-09 14:57:46 +0000

    Add a couple of helper functions.

--- python/src/LpdFemTests/LpdProtoTest.py
+++ python/src/LpdFemTests/LpdProtoTest.py
@@ -29,15 +29,39 @@ class LpdProtoTest(FemClient):
         Get temperature value (in ADC counts) of channel in device
         '''
         
-        pass
-    
+        adcValue = self.ad7998read(device, channel)
+        tempValue = self.adcToTemp(adcValue)
+        
     def SensorATempSet(self, device, channel, value):
         '''
         Set temperature value of channel in device
         '''
         
+        adcValue = self.tempToAdc(value)
+        ack = self.ad7998write(device, channel, adcValue)
+    
+
+    """ -=-=-=-=-=- Helper Functions -=-=-=-=-=- """
+    
+    def ad7998read(self, device, channel):
+        '''
+        '''
         pass
     
+    def adcToTemp(self, adcValue):
+        '''
+        '''
+        pass
+
+    def tempToAdc(self, value):
+        '''
+        '''
+        pass
+
+    def ad7998write(self, device, channel, adcValue):
+        '''
+        '''
+        pass
 
 
 if __name__ == "__main__":
