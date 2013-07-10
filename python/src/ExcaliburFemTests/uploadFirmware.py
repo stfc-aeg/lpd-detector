b872f1fbdcce8f23c8b4a18db64ee6c8494fa72f
mt47 <mt47>
2013-07-10 13:34:35 +0000

    Added command line parameters, better upload progress feedback

--- python/src/ExcaliburFemTests/uploadFirmware.py
+++ python/src/ExcaliburFemTests/uploadFirmware.py
@@ -9,96 +9,117 @@ Created on June 24 - 2013
 
 @author: mt47
 '''
-import sys, os.path, time, binascii
+import sys, os.path, time, binascii, argparse
 
 import ExcaliburFemTests.defaults as defaults
 from FemClient.FemClient import *
 from FemApi.FemSysaceConfig import FemSysaceConfig
+import ExcaliburFemTests
 
-defaults.parseArgs()
-crc = 0
+def uploadFirmware(femHost, femPort, filename, desc, slot):
 
-# Configuration defaults
-chunkSize = 768432
-
-# Put these somewhere better
-CMD_WRITE_TO_CF = 2
-CMD_GET_FWADDR = 4
-
-# TODO: Accept these as parameters!
-# ----------------------------------
-# 'Firmware' file to upload
-filename = "../../rev0.ace"
-
-# Description
-desc = "TEST_UPLOAD"
-
-# SystemACE slot
-slot = 1
-# ----------------------------------
-
-# Get file size & last modified date
-size = os.path.getsize(filename)
-import struct
-
-fileTime = int(os.path.getmtime(filename))
-
-if (size==0):
-    print "FILE EMPTY, ABORTING"
-    sys.exit(1)
-
-# Connect to FEM
-try:
-    theFem = FemClient((defaults.femHost, defaults.femPort), defaults.femTimeout)
-except FemClientError as errString:
-    print "Error: FEM connection failed:", errString
-    sys.exit(1)
-
-# Query FEM where to upload metadata / image bundle to
-response = theFem.commandSend(CMD_GET_FWADDR, 0);
-addr = response.payload[1]
-cfgAddr = addr
-print ""
-print "Upload address:      "+ hex(addr)
-print "SystemACE slot:     ", slot
-print "Image size (bytes): ", size
-
-uploadTime = time.time()
-addr += FemSysaceConfig.configSize()
-
-# Chunk and upload image to addr + sizeof(metadata)
-f = open(filename, 'rb')
-if not f:
-    print "Cannot open " + filename
-    sys.exit(1)
+    crc = 0
     
-print "Chunk size (bytes): ", chunkSize
+    if (femHost == None):
+        print "Error: No FEM address specified!"
+        sys.exit(1)
+    if (femPort == None):
+        print "Error: No FEM port specified!"
+        sys.exit(1)
+    if (slot == None):
+        print "Error: No FEM SystemACE slot specified!"
+        sys.exit(1)
+    
+    # Configuration defaults
+    chunkSize = 768432
+    CMD_WRITE_TO_CF = 2
+    CMD_GET_FWADDR = 4
+    
+    # Get file size & last modified date
+    size = os.path.getsize(filename)
+    import struct
+    
+    fileTime = int(os.path.getmtime(filename))
+    
+    if (size == 0):
+        print "Error: Can't open file or file empty!"
+        sys.exit(1)
+    
+    # Connect to FEM
+    try:
+        theFem = FemClient((femHost, femPort), defaults.femTimeout)
+    except FemClientError as errString:
+        print "Error: FEM connection failed:", errString
+        sys.exit(1)
+    
+    # Query FEM where to upload metadata / image bundle to
+    response = theFem.commandSend(CMD_GET_FWADDR, 0);
+    addr = response.payload[1]
+    cfgAddr = addr
+    print ""
+    print "Upload address:      " + hex(addr)
+    print "SystemACE slot:     ", slot
+    print "Image size (bytes): ", size
+    
+    uploadTime = time.time()
+    addr += FemSysaceConfig.configSize()
+    
+    # Chunk and upload image to addr + sizeof(metadata)
+    f = open(filename, 'rb')
+    if not f:
+        print "Cannot open " + filename
+        sys.exit(1)
+        
+    print "Chunk size (bytes): ", chunkSize
+    
+    print "Uploading:          ",
+    while True:
+        chunk = f.read(chunkSize)
+        if not chunk: break
+    
+        payload = [ord(val) for val in chunk]
+        theFem.directWrite(theAddr=addr, thePayload=payload)
+        addr += chunkSize
+    
+        crc = binascii.crc32(chunk, crc)    
+        
+        print "#",
+        sys.stdout.flush()
+    
+    f.close()
+    
+    print ""
+    
+    crc = crc & 0xFFFFFFFF
+    
+    finishTime = time.time()
+    duration = finishTime - uploadTime
+    print "Upload time:         " + str(duration) + "s"
+    print "Upload speed:        " + str((size / duration) / 1024) + "kb/s, " + str((size / duration) / 1024 / 1024) + "Mb/s"
+    print "CRC32:               " + hex(crc)
+    
+    # Upload metadata with updated crc
+    config = FemSysaceConfig(FemSysaceConfig.HEADER_MAGIC_WORD, size, slot, desc, fileTime, int(uploadTime), 0, crc, 0);
+    configPayload = config.decodeAsInt()
+    theFem.rawWrite(theAddr=cfgAddr, thePayload=configPayload)
+    
+    # Issue firmware upload command
+    theFem.commandSend(CMD_WRITE_TO_CF, 0)
+    
+    return
 
-print "UPLOADING..."
-while True:
-    chunk = f.read(chunkSize)
-    if not chunk: break
+# ----------------------
 
-    payload = [ord(val) for val in chunk]
-#    theFem.directWrite(theAddr=addr, thePayload=payload)
-    addr += chunkSize
 
-    crc = binascii.crc32(chunk, crc)    
+if __name__ == "__main__":
 
-f.close()
+    # Create parser object and arguments, including defaults
+    parser = argparse.ArgumentParser(description="uploadFirmware.py - Uploads SystemACE firmware image to remote FEM.", epilog="All parameters other than femhost / femport required.")
+    parser.add_argument("--file", "-f", help="SystemACE firmware file", type=str, required=True, action='store')
+    parser.add_argument("--slot", "-s", help="SystemACE slot number", type=int, choices=range(8), required=True, action='store')
+    parser.add_argument("--desc", "-d", help="Description string for image", type=str, required=True, action='store')
+    parser.add_argument("--address", "-a", help="[Optional] Set fem host IP (e.g. 10.0.0.1)", type=str, default=defaults.femHost, action='store')
+    parser.add_argument("--port", "-p", help="[Optional] Set fem port (e.g. 6969)", type=int, default=defaults.femPort, action='store')
+    args = parser.parse_args()
 
-crc = crc &0xFFFFFFFF
-
-finishTime = time.time()
-duration = finishTime - uploadTime
-print "Upload time:         " + str(duration) + "s"
-print "Upload speed:        " + str((size/duration)/1024) + "kb/s, " + str((size/duration)/1024/1024) + "Mb/s"
-print "CRC32:               " + hex(crc)
-
-# Upload metadata with updated crc
-config = FemSysaceConfig(FemSysaceConfig.HEADER_MAGIC_WORD, size, slot, desc, fileTime, int(uploadTime), 0, crc, 0);
-configPayload = config.decodeAsInt()
-theFem.rawWrite(theAddr=cfgAddr, thePayload=configPayload)
-
-# Issue firmware upload command
-#theFem.commandSend(CMD_WRITE_TO_CF, 0)
+    uploadFirmware(args.address, args.port, args.file, args.desc, args.slot)
