96caba426e494205426f8c50d7392fcce5149778
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2017-04-18 16:11:34 +0100

    More unit tests for excalibur.detector (incomplete coverage)

--- excalibur/testing/test_detector.py
+++ excalibur/testing/test_detector.py
@@ -54,3 +54,40 @@ class TestExcaliburDetector():
         detector = ExcaliburDetector(self.detector_fems)
         connect_params = {'state': True}
         detector.connect(connect_params)
+
+    def test_detector_powercard_idx(self):
+        
+        detector = ExcaliburDetector(self.detector_fems)
+        powercard_idx = 1
+        detector.set_powercard_fem_idx(powercard_idx)
+        assert_equal(detector.powercard_fem_idx, powercard_idx)
+        
+    def test_detector_bad_powercard_idx(self):
+        
+        detector = ExcaliburDetector(self.detector_fems)
+        powercard_idx = 4
+        with assert_raises_regexp(
+            ExcaliburDetectorError, "Illegal FEM index {} specified for power card".format(powercard_idx)
+        ):
+            detector.set_powercard_fem_idx(powercard_idx)
+
+    def test_detector_set_chip_enable_mask(self):
+        
+        detector = ExcaliburDetector(self.detector_fems)
+        chip_enable_mask = [0xff, 0x3f, 0x7f]
+        detector.set_chip_enable_mask(chip_enable_mask)
+        assert_equal(chip_enable_mask, detector.chip_enable_mask)
+        
+    def test_detector_set_chip_enable_mask_single(self):
+        
+        detector = ExcaliburDetector(('192.168.0.1', 6969, '10.0.2.1'))
+        chip_enable_mask = 0xff
+        detector.set_chip_enable_mask(chip_enable_mask)
+        assert_equal([chip_enable_mask], detector.chip_enable_mask)
+        
+    def test_detector_set_chip_enable_length_mistmatch(self):
+        
+        detector = ExcaliburDetector(self.detector_fems)
+        chip_enable_mask = [0xff, 0x3f]
+        with assert_raises_regexp(ExcaliburDetectorError, 'Mismatch in length of asic enable mask'):
+            detector.set_chip_enable_mask(chip_enable_mask)
