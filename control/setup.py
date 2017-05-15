f917489899ef071287e7c31fed96ffd6ae5ee1d3
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2017-05-15 09:57:29 +0100

    Updated odin dependency URL to match new odin-detector github organisation

--- control/setup.py
+++ control/setup.py
@@ -136,7 +136,7 @@ setup(
     ext_modules=fem_ext_modules,
     packages=find_packages(),
     install_requires=['odin==0.2'],
-    dependency_links=['https://github.com/percival-detector/odin/zipball/0.2#egg=odin-0.2'],
+    dependency_links=['https://github.com/odin-detector/odin-control/zipball/0.2#egg=odin-0.2'],
     extras_require={
       'test': ['nose', 'coverage', 'mock'],  
     },
