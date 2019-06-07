97e9448df8a51b6d7ff88f990718f239a958a7dc
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2019-06-07 09:44:28 +0100

    Add lpd_fem_gui console entry point to setup.py and tidy formatting

--- app/setup.py
+++ app/setup.py
@@ -7,13 +7,18 @@ with open('requirements.txt') as f:
    required = f.read().splitlines()
 
 setup(name='lpd',
-      version=versioneer.get_version(),
-      cmdclass=versioneer.get_cmdclass(),
-      description='STFC LPD',
-      url='https://github.com/stfc-aeg/lpd-detector.git',
-      author='Tim Nicholls',
-      author_email='tim.nicholls@stfc.ac.uk',
-      packages=find_packages(),
-      install_requires=required,
-      zip_safe=False
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description='STFC LPD GUI Framework',
+    url='https://github.com/stfc-aeg/lpd-detector.git',
+    author='Tim Nicholls',
+    author_email='tim.nicholls@stfc.ac.uk',
+    packages=find_packages(),
+    install_requires=required,
+    entry_points = {
+      'console_scripts': [
+            'lpd_fem_gui = lpd.gui.gui:main',
+      ]
+     },
+     zip_safe=False,
 )
